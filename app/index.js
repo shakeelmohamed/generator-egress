b9776c1b224736655738f5f398448c71aa90f1ae
Shakeel Mohamed <smohamed@splunk.com>
2014-01-11 22:51:23 -0800

    notes n' stuff

--- app/index.js
+++ app/index.js
@@ -25,42 +25,51 @@ NopejsGenerator.prototype.askFor = function askFor() {
 
   var prompts = [
     {
-      name: 'packageName',
-      message: 'What would you like to name this package? (this will be slugified)'
-    },
-    {
       name: 'siteName',
       message: 'What would you like to call your site?'
     },
     {
       name: 'siteAuthor',
       message: 'Who is the site author?'
-    }
+    },
+    {
+      name: 'siteDescription',
+      message: 'Describe your site:'
+    },
   ];
 
   this.prompt(prompts, function (props) {
-    this.packageName = props.packageName;
     this.siteName = props.siteName;
     this.siteAuthor = props.siteAuthor;
+    this.siteDescription = props.siteDescription;
 
     cb();
   }.bind(this));
 };
 
 NopejsGenerator.prototype.app = function app() {
-  this.copy('_package.json', 'package.json');
-  this.copy('.bowerrc');
+  //TODO: this is broken! The nope.js package overwrite it :(
+  this.template('_package.json', 'package.json');
+  //TODO: add in a readme.md with pre-production steps, and maybe some tests to verify
+    //... these include: setting a better secret, siteName, siteAuthor, etc.
 };
 
 NopejsGenerator.prototype.gruntfile = function gruntfile() {
   this.template('Gruntfile.js');
 };
 
+/*
 NopejsGenerator.prototype.projectfiles = function projectfiles() {
   this.copy('jshintrc', '.jshintrc');
 };
+*/
 
 NopejsGenerator.prototype.nopejsFiles = function nopejsFiles() {
+  //TODO: remove this bower installation, make nopejs a dependency for the generator
+    //... then do a manual copy process, with templating commands in some config file, etc.
+    //... this means the nope.js config needs to be externalized slightly from the web.js file.
+    //... don't copy over the nope.js package.json file.
+
   this.bowerInstall("shakeelmohamed/nopejs", { save: true }, function(){
     fs.copy('./bower_components/nopejs', './', function (err) {
       if (err) {
@@ -70,7 +79,7 @@ NopejsGenerator.prototype.nopejsFiles = function nopejsFiles() {
         fs.remove('./bower_components', function(err){
           if (err) return console.error(err);
 
-          console.log("success!")
+          console.log("success!");
         });
       }
     });
